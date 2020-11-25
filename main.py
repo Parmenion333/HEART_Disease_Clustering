cb2ea9122e9d280b45690f3e9210ce4c6b66da11
Parmenion333 <justinsams98@yahoo.de>
2020-11-25 21:05:39 +0100

    Justin_25/11/2020_21:03
    - information.txt angelegt mit Infos zu Variables
    - main.py angelegt und datensatz importiert. Außerdem in kategorielle und numerische Variablen unterteilt
    - visualizations.py erstellt und histogramme zu kat. var. erstellt (# plt.show())

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = pd.read_csv("heart.csv")
+
+#print(data.head(5))
+#print(data.info())
+#print(data.describe())
+
+# making dataframe consiting of categorical variables 
+df_categorical = data[["sex","fbs","exang","cp","restecg","slope","thal","target"]]
+
+# making dataframe consisting of numerical varibles
+df_numeric = data[["age","trestbps","chol","thalach","oldpeak","ca"]]
---
+++ visualization.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = pd.read_csv("heart.csv")
+
+print(data.head(5))
+print(data.info())
+print(data.describe())
+
+# making dataframe consiting of categorical variables 
+df_categorical = data[["sex","fbs","exang","cp","restecg","slope","thal","target"]]
+
+# making dataframe consisting of numerical varibles
+df_numeric = data[["age","trestbps","chol","thalach","oldpeak","ca"]]
+
+#df_categorical.head(5)
+
+# catgorical variables visualisation with histogramms 
+
+plt.hist(df_categorical["sex"], color = "blue")
+plt.xlabel("Women - Men")
+plt.ylabel("Number")
+plt.title("Gender distribution")
+#plt.show()
+
+plt.hist(df_categorical["fbs"], color = "yellow")
+plt.xlabel("false - true")
+plt.ylabel("Number")
+plt.title("Fasting blood sugar")
+#plt.show()
+
+plt.hist(df_categorical["exang"], color = "purple")
+plt.xlabel("false - true")
+plt.ylabel("Number")
+plt.title("Exercise induced angina")
+#plt.show()
+
+plt.hist(df_categorical["cp"], color = "green")
+plt.xlabel("from 0 to 3")
+plt.ylabel("Number")
+plt.title("Chest pain type")
+#plt.show()
+
+plt.hist(df_categorical["slope"], color = "brown")
+plt.xlabel("descending - flat - ascending")
+plt.ylabel("Number")
+plt.title("ST segment slope")
+#plt.show()
+
+plt.hist(df_categorical["thal"], color = "pink")
+plt.xlabel("NaN - fixed - normal - reversable")
+plt.ylabel("Number")
+plt.title("Thallium")
+#plt.show()
+
+plt.hist(df_categorical["restecg"], color = "black")
+plt.xlabel("...")
+plt.ylabel("Number")
+plt.title("Rest ECG")
+#plt.show()
+
+plt.hist(df_categorical["target"], color = "red")
+plt.xlabel("no disease - disease")
+plt.ylabel("Number")
+plt.title("Disease")
+#plt.show()
+
