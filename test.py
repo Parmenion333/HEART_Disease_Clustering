d72386f97e7b01b088623d0b00a4762d273b7c6e
Parmenion333 <justinsams98@yahoo.de>
2020-11-25 22:26:06 +0100

    Justin_25/11/2020_22:25
    Jointplots für alle Variablen in abhängigkeit vom Geschlecht erstellt
    
    und alles ausgeklammert
    
    (shift + alt + a)

--- test.py
+++ test.py
@@ -5,15 +5,18 @@ import seaborn as sns
 
 data = pd.read_csv("heart.csv")
 
-#print(data.head(5))
-#print(data.info())
-#print(data.describe())
-
 # making dataframe consiting of categorical variables 
 df_categorical = data[["sex","fbs","exang","cp","restecg","slope","thal","target"]]
 
 # making dataframe consisting of numerical varibles
 df_numeric = data[["age","trestbps","chol","thalach","oldpeak","ca"]]
 
+#print(df_categorical.loc[df_categorical["sex"] == 1])
 
+sns.jointplot(x="sex", y="chol", data= data)
+#plt.show()
 
+for i in df_categorical.columns:
+    sns.jointplot(x="sex", y= i, data=data)
+    plt.title(i)
+    plt.show()
--- visualization.py
+++ visualization.py
@@ -19,51 +19,65 @@ df_numeric = data[["age","trestbps","chol","thalach","oldpeak","ca"]]
 
 # catgorical variables visualisation with histogramms 
 
-plt.hist(df_categorical["sex"], color = "blue")
+""" plt.hist(df_categorical["sex"], color = "blue")
 plt.xlabel("Women - Men")
 plt.ylabel("Number")
 plt.title("Gender distribution")
-#plt.show()
+plt.show()
 
 plt.hist(df_categorical["fbs"], color = "yellow")
 plt.xlabel("false - true")
 plt.ylabel("Number")
 plt.title("Fasting blood sugar")
-#plt.show()
+plt.show()
 
 plt.hist(df_categorical["exang"], color = "purple")
 plt.xlabel("false - true")
 plt.ylabel("Number")
 plt.title("Exercise induced angina")
-#plt.show()
+plt.show()
 
 plt.hist(df_categorical["cp"], color = "green")
 plt.xlabel("from 0 to 3")
 plt.ylabel("Number")
 plt.title("Chest pain type")
-#plt.show()
+plt.show()
 
 plt.hist(df_categorical["slope"], color = "brown")
 plt.xlabel("descending - flat - ascending")
 plt.ylabel("Number")
 plt.title("ST segment slope")
-#plt.show()
+plt.show()
 
 plt.hist(df_categorical["thal"], color = "pink")
 plt.xlabel("NaN - fixed - normal - reversable")
 plt.ylabel("Number")
 plt.title("Thallium")
-#plt.show()
+plt.show()
 
 plt.hist(df_categorical["restecg"], color = "black")
 plt.xlabel("...")
 plt.ylabel("Number")
 plt.title("Rest ECG")
-#plt.show()
+plt.show()
 
 plt.hist(df_categorical["target"], color = "red")
 plt.xlabel("no disease - disease")
 plt.ylabel("Number")
 plt.title("Disease")
-#plt.show()
+plt.show() """
 
+# jointplot comparing all categorical variables depending on gender
+
+""" for i in df_categorical.columns:
+    sns.jointplot(x="sex", y= i, data=data)
+    plt.title(i)
+    #plt.show() """
+
+
+# jointplot comapring all numerical variables depending on gender
+
+for a in df_numeric.columns:
+    sns.jointplot(x=df_categorical["sex"], y= a, data=data)
+    plt.title(a)
+    plt.show()
