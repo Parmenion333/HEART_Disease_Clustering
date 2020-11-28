3746298d043e82e90734b1559186de4e9bf4283d
Parmenion333 <justinsams98@yahoo.de>
2020-11-28 18:36:49 +0100

    Justin_28/11/2020_18:36
    - Repair

--- pandas_analysis.py
+++ pandas_analysis.py
@@ -10,3 +10,18 @@ print(data.pivot_table(values=["chol","target"], index="sex"))
 
 
 print((data["sex"] == 1).count())
+
+target_series = data["target"]
+sex_series = data["sex"]
+
+# pivot_table index = sex
+print(data.pivot_table(values=["fbs","exang","cp","restecg","slope","thal","target","age","trestbps","chol","thalach","oldpeak","ca"], index="sex"))
+
+# Unterschiede von mind. 0.25 bei ca, chol, oldpeak, target??, thal, cp zwischen Männern und Frauen
+# pivot_table index = target
+print(data.pivot_table(values=["fbs","exang","cp","restecg","slope","thal","sex","age","trestbps","chol","thalach","oldpeak","ca"], index="target"))
+# correlation between sex and target -0.28
+print(sex_series.corr(target_series, method = "pearson"), "correlation between sex and target")
+
+# correlation matrix of data 
+print(data.corr(method = "pearson"))
